e313bbdba27d2912361ba408de3256d335506053
MoniratnaNord <moniratna@nordfinance.io>
2025-06-26 17:18:57 +0530

    fixed logo and added toast

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,10 +6,10 @@ export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<>
 			<Head>
-				<title>Tensai Kit</title>
+				<title>Tensai Kit - The Ultimate AI Toolkit for Developers</title>
 				<meta name="description" content="This is a description for SEO." />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				{/* <link rel="icon" href="/favicon.ico" /> */}
+				<link rel="icon" href="/iconyellowcircle.png" />
 			</Head>
 			<Component {...pageProps} />
 		</>
